98742ca375e163ecfe6ef319ba534f8ab1f02537
usausa <machi.pon@gmail.com>
2023-03-22 09:50:42 +0900

    Remove sandbox

--- Sandbox/SenseHatTest/Program.cs
+++
@@ -1,9 +0,0 @@
-using Iot.Device.SenseHat;
-
-#pragma warning disable CA1852
-
-using var senseHat = new SenseHat();
-Console.WriteLine(senseHat.Temperature.Value);
-Console.WriteLine(senseHat.Temperature2.Value);
-Console.WriteLine(senseHat.TemperatureAndHumidity.Temperature.Value);
-Console.WriteLine(senseHat.TemperatureAndHumidity.Humidity.Value);
